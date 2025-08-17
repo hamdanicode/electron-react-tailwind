8a0ecba25c2404962e85d763d0d273d37a4cba11
hamdanicode <hamdani.inf@gmail.com>
2025-08-17 11:36:55 +0700

    [fix] stayling

--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -3,17 +3,24 @@ import type { Configuration } from 'webpack';
 import { rules } from './webpack.rules';
 import { plugins } from './webpack.plugins';
 
+// rules.push({
+//   test: /\.css$/,
+//   use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+// });
 rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+  test: /\.s[ac]ss$/i, // Matches Sass/SCSS files
+  use: [
+    'style-loader', // Injects CSS directly into the DOM (for development)
+    'css-loader', // Converts CSS into CommonJS modules
+    'postcss-loader', // Compiles Sass/SCSS to CSS using PostCSS plugins
+  ],
 });
-
 export const rendererConfig: Configuration = {
   module: {
     rules,
   },
   plugins,
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css','.scss'],
   },
 };
